0cc5ebfab7b2a326200af501073c8d85df390641
Pedro <pedro.freitas@serpro.gov.br>
2024-11-28 11:03:44 -0300

    Lógica de programação - JS

---
+++ app.js
@@ -0,0 +1,10 @@
+alert('Olá mundo');
+let numeroSecreto = 8;
+let chute = prompt('Escolha um número entre 1 é 10');
+
+
+if (numeroSecreto == chute){
+    alert(`Você acertou! O número é:${ numeroSecreto}`);
+}else{
+     alert('Você errou!');
+}
